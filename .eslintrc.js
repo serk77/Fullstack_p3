89c2e3d0ff6c766dc1c0b825867890948613ae9a
serk_77 <54988105+serk77@users.noreply.github.com>
2022-02-26 16:47:04 +0200

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    	"no-unused-vars": "off",
+    	"no-mixed-spaces-and-tabs" : "off",
+    	"no-undef": "off"
+    }
+}
